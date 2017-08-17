cb9fb65464248d4a8a79f7d5151793eedac8500d
Henry Curtis <hcuk106@gmail.com>
2017-08-18 00:47:43 +0100

    Changes made to include Top five posts view as well as Navbar

--- blog_prj/blog/urls.py
+++ blog_prj/blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^(?P<id>\d+)/$', views.post_detail),
     url(r'^top5posts/$', views.top_post),
+    url(r'^top5posts/(?P<id>\d+)/$', views.post_detail),
     url(r'^blog/$', views.post_list),
     url(r'^blog/(?P<id>\d+)/$', views.post_detail),
 ]
