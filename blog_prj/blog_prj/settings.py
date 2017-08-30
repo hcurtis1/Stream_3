934319b17a406a5b07dc759fab46b5356f626110
Henry Curtis <hcuk106@gmail.com>
2017-08-30 21:37:28 +0100

    Media Roots added as well as django forms bootstrap

--- blog_prj/blog_prj/settings.py
+++ blog_prj/blog_prj/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'django.contrib.sites',
     'disqus',
+    'django_forms_bootstrap',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ STATICFILES_DIRS = (
 # Disqus admin
 DISQUS_WEBSITE_SHORTNAME = 'BootCampBlog'
 SITE_ID = 1
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
