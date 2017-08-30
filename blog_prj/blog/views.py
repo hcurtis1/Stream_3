e38f81eaf528ae24261c3266d58b75b868d245a0
Henry Curtis <hcuk106@gmail.com>
2017-08-30 21:36:24 +0100

    Edit post and new post views added

--- blog_prj/blog/views.py
+++ blog_prj/blog/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from .forms import BlogPostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -31,3 +33,32 @@ def post_detail(request, id):
 def top_post(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-views')[:5]
     return render(request, "blogposts.html", {'posts': posts})
+
+
+def new_post(request):
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect(post_detail, post.pk)
+    else:
+        form = BlogPostForm()
+    return render(request, 'blogpostform.html', {'form': form})
+
+
+def edit_post(request, id):
+    post = get_object_or_404(Post, pk=id)
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect(post_detail, post.pk)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blogpostform.html', {'form': form})
