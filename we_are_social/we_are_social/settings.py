a0a69b5d321ab3b65f332d3f54603e99f6ef05ad
Henry Curtis <hcuk106@gmail.com>
2017-09-27 23:41:13 +0100

    Backend duplicates removed

--- we_are_social/we_are_social/settings.py
+++ we_are_social/we_are_social/settings.py
@@ -113,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'accounts.User'
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
     'accounts.backends.EmailAuth',
 )
 
