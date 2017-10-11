a68cd01a7860708caa2cb11b7084d42b8d83a859
Henry Curtis <hcuk106@gmail.com>
2017-10-11 18:54:30 +0100

    Replacing for changed name

--- we_are_social/magazines/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-import views as magazine_views
-
-urlpatterns = [
-    url(r'^magazines/$', magazine_views.all_magazines),
-]
--- we_are_social/paypal_store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from paypal.standard.ipn import urls as paypal_urls
-import views as paypal_views
-
-urlpatterns = [
-    url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
-    url(r'^paypal-return', paypal_views.paypal_return),
-    url(r'^paypal-cancel', paypal_views.paypal_cancel),
-]
